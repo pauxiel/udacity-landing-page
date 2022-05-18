ef1f602e5593db1921398733a25ebc3c2b2761ac
pauxiel <paul.scholes50@yahoo.com>
2022-05-18 23:21:02 +0100

    new correction parttwo

--- app/script.js
+++ app/script.js
@@ -1,20 +1,34 @@
-// create an unordered list
+// selected the sections
+const allSections = Array.from(document.getElementsByTagName("section"));
 
-const items = document.createElement("ul");
-items.classList.add("nav__list");
+// create an array for section excluding hero section
+const selectSections = allSections.filter(
+  (section) => section.className.length === 0
+);
 
-// create the list items
-const firstItem = document.createElement("li");
-const secondItem = document.createElement("li");
-const thirdItem = document.createElement("li");
-const fourthItem = document.createElement("li");
+// select nav list
+const navList = document.getElementsByClassName("nav__list");
 
-// Add the class nav__item to the list item and add the class active when clicked
-const listItem = [firstItem, secondItem, thirdItem, fourthItem];
-for (const item of listItem) {
-  item.classList.add("nav__item");
 
-  item.addEventListener("click", function(e) {
+//---------------------------------------//
+// Dynamic creation of the nav list----//
+//-----------------------------------//
+
+for (section of selectSections) {
+  // create list item with class
+  const listItem = document.createElement("li");
+  listItem.classList.add("nav__item");
+
+  // create list item link with class
+  const listItemLink = document.createElement("a");
+  listItemLink.classList.add("nav__link");
+
+  // create text content and href for list item link
+  listItemLink.textContent = section.dataset.nav;
+  listItemLink.href = `#${section.id}`;
+
+  // create highlight action when list item is clicked
+  listItem.addEventListener("click", function(e) {
     e.preventDefault();
     const current = document.getElementsByClassName("highlight");
 
@@ -24,43 +38,16 @@ for (const item of listItem) {
 
     this.className += " highlight";
   });
+
+  listItem.appendChild(listItemLink);
+
+  navList[0].appendChild(listItem);
 }
 
-// create the nav link with its text content and link
-const firstAnchor = document.createElement("a");
-firstAnchor.textContent = "Section-1";
-firstAnchor.href = "#section1";
+//--------------------------------------------------------------//
+// smooth scrolling to the section when the nav link is clicked--//
+//--------------------------------------------------------------//
 
-const secondAnchor = document.createElement("a");
-secondAnchor.textContent = "Section-2";
-secondAnchor.href = "#section2";
-
-const thirdAnchor = document.createElement("a");
-thirdAnchor.textContent = "Section-3";
-thirdAnchor.href = "#section3";
-
-const fourthAnchor = document.createElement("a");
-fourthAnchor.textContent = "Section-4";
-fourthAnchor.href = "#section4";
-
-// add the class nav__link to all the link in the nav
-const listAnchor = [firstAnchor, secondAnchor, thirdAnchor, fourthAnchor];
-for (const anchor of listAnchor) {
-  anchor.classList.add("nav__link");
-}
-
-// append all the nav link to the nav list item
-for (let i = 0; i < listItem.length; i++) {
-  listItem[i].appendChild(listAnchor[i]);
-  console.log(listItem[1]);
-}
-
-// append the nav list item to the nav
-items.append(firstItem, secondItem, thirdItem, fourthItem);
-const nav = document.getElementsByClassName("nav");
-nav[0].appendChild(items);
-
-// smooth scrolling to the section when the nav link is clicked
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function(e) {
     e.preventDefault();
@@ -71,22 +58,23 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-const section = document.querySelectorAll("section");
+//-----------------------------//
+// add the active state--------//
+//------------------------------//
 
+window.addEventListener("scroll", (e) => {
+  // loop through each section
+  selectSections.forEach((section) => {
+    // get px distance from top
+    const topDistance = section.getBoundingClientRect().top;
 
-// add the class active to section
-window.addEventListener("scroll", function(e) {
-  e.preventDefault();
-  section.forEach(function(elem) {
-    const distance = elem.getBoundingClientRect();
-    // console.log(distance)
-    if (
-      distance.top >= 0 &&
-      distance.left >= 0 &&
-      distance.bottom <= document.documentElement.clientHeight &&
-      distance.right <= document.documentElement.clientWidth
-    ) {
-      elem.classList.add("active");
-    } else elem.classList.remove("active");
+    // if the distance to the top is between 0-100px
+    if (topDistance > 0 && topDistance < 100) {
+      section.classList.add("active");
+
+      // otherwise, remove the class
+    } else {
+      section.classList.remove("active");
+    }
   });
 });
